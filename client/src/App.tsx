046831a459eaa98cc85d57a2608ba524ca6697ac
Qelmi <33161152+SweetAzRain@users.noreply.github.com>
2025-08-02 20:34:18 +0300

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,6 +3,7 @@ import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
+import { NearWalletProvider } from "@/hooks/use-near-wallet";
 import MintPage from "@/pages/mint";
 import NotFound from "@/pages/not-found";
 
